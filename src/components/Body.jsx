bfa0518c68534beb542dc10be6fe6315809afded
DausHamyR <daus.amat354@gmail.com>
2023-09-05 18:15:54 +0700

    update

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,7 +6,7 @@ import {TfiMenuAlt} from 'react-icons/tfi'
 
 function Body() {
     return (
-        <div className='bg-white w-[97%] mx-4 mt-6 p-6'>
+        <div>
             <div className='flex justify-between mb-12'>
                 <div className='font-medium text-xl'>
                     {location.pathname === '/paneladmin/kategori' && <div className='flex items-center gap-2'><MdSell />Data Kategori</div>}
@@ -23,7 +23,16 @@ function Body() {
                     {location.pathname === '/paneladmin/ourservice' && <div className='flex items-center gap-2'><AiOutlineFileText />Data Ourservice</div>}
                 </div>
                 <div className='bg-[#3bc0c3] px-2 py-0.5 cursor-pointer hover:bg-[#30999c]'>
-                    <div className='text-white font-medium'>+ Tambah Kategori</div>
+                    {location.pathname === '/paneladmin/kategori' && <div className='text-white font-medium'>+ Tambah Kategori</div>}
+                    {location.pathname === '/paneladmin/halaman' && <div className='text-white font-medium'>+ Tambah Halaman</div>}
+                    {location.pathname === '/paneladmin/acara' && <div className='text-white font-medium'>+ Tambah Acara</div>}
+                    {location.pathname === '/paneladmin/article' && <div className='text-white font-medium'>+ Tambah Article</div>}
+                    {location.pathname === '/paneladmin/album' && <div className='text-white font-medium'>+ Tambah Album</div>}
+                    {location.pathname === '/paneladmin/galeri' && <div className='text-white font-medium'>+ Tambah Galeri</div>}
+                    {location.pathname === '/paneladmin/vidio' && <div className='text-white font-medium'>+ Tambah Vidio</div>}
+                    {location.pathname === '/paneladmin/slider' && <div className='text-white font-medium'>+ Tambah Slider</div>}
+                    {location.pathname === '/paneladmin/menu' && <div className='text-white font-medium'>+ Tambah Menu Utama</div>}
+                    {location.pathname === '/paneladmin/ourservice' && <div className='text-white font-medium'>+ Tambah Ourservice</div>}
                 </div>
             </div>
             <div className='flex justify-between mb-4'>
@@ -37,154 +46,6 @@ function Body() {
                 <div className='flex items-center gap-2'>
                     <div>Search:</div>
                     <input type="text" className='border-2 pl-2'/>
-                </div>
-            </div>
-            <div>
-                <div className='flex justify-between font-medium'>
-                    <div className='flex-[0.05] border py-2 px-4 '>No</div>
-                    <div className='flex-[0.22] border py-2 px-4 '>Kategori</div>
-                    <div className='flex-[0.48] border py-2 px-4 '>Slug</div>
-                    <div className='flex-[0.25] border py-2 px-4 '>Aksi</div>
-                </div>
-                <div className='flex justify-between font-medium'>
-                    <div className='flex-[0.05] border py-2 px-4 '>1</div>
-                    <div className='flex-[0.22] border py-2 px-4 '>Kategori</div>
-                    <div className='flex-[0.48] border py-2 px-4 '>Slug</div>
-                    <div className='flex-[0.25] border py-2 px-4 flex gap-2 text-white'>
-                        <div className='bg-[#edc755] px-2 py-0.5 cursor-pointer'>
-                            <div>Edit</div>
-                        </div>
-                        <div className='bg-red-600 px-2 py-0.5 cursor-pointer'>
-                            <div>Hapus</div>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex justify-between font-medium'>
-                    <div className='flex-[0.05] border py-2 px-4 '>2</div>
-                    <div className='flex-[0.22] border py-2 px-4 '>Kategori</div>
-                    <div className='flex-[0.48] border py-2 px-4 '>Slug</div>
-                    <div className='flex-[0.25] border py-2 px-4 flex gap-2 text-white'>
-                        <div className='bg-[#edc755] px-2 py-0.5 cursor-pointer'>
-                            <div>Edit</div>
-                        </div>
-                        <div className='bg-red-600 px-2 py-0.5 cursor-pointer'>
-                            <div>Hapus</div>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex justify-between font-medium'>
-                    <div className='flex-[0.05] border py-2 px-4 '>3</div>
-                    <div className='flex-[0.22] border py-2 px-4 '>Kategori</div>
-                    <div className='flex-[0.48] border py-2 px-4 '>Slug</div>
-                    <div className='flex-[0.25] border py-2 px-4 flex gap-2 text-white'>
-                        <div className='bg-[#edc755] px-2 py-0.5 cursor-pointer'>
-                            <div>Edit</div>
-                        </div>
-                        <div className='bg-red-600 px-2 py-0.5 cursor-pointer'>
-                            <div>Hapus</div>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex justify-between font-medium'>
-                    <div className='flex-[0.05] border py-2 px-4 '>4</div>
-                    <div className='flex-[0.22] border py-2 px-4 '>Kategori</div>
-                    <div className='flex-[0.48] border py-2 px-4 '>Slug</div>
-                    <div className='flex-[0.25] border py-2 px-4 flex gap-2 text-white'>
-                        <div className='bg-[#edc755] px-2 py-0.5 cursor-pointer'>
-                            <div>Edit</div>
-                        </div>
-                        <div className='bg-red-600 px-2 py-0.5 cursor-pointer'>
-                            <div>Hapus</div>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex justify-between font-medium'>
-                    <div className='flex-[0.05] border py-2 px-4 '>5</div>
-                    <div className='flex-[0.22] border py-2 px-4 '>Kategori</div>
-                    <div className='flex-[0.48] border py-2 px-4 '>Slug</div>
-                    <div className='flex-[0.25] border py-2 px-4 flex gap-2 text-white'>
-                        <div className='bg-[#edc755] px-2 py-0.5 cursor-pointer'>
-                            <div>Edit</div>
-                        </div>
-                        <div className='bg-red-600 px-2 py-0.5 cursor-pointer'>
-                            <div>Hapus</div>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex justify-between font-medium'>
-                    <div className='flex-[0.05] border py-2 px-4 '>6</div>
-                    <div className='flex-[0.22] border py-2 px-4 '>Kategori</div>
-                    <div className='flex-[0.48] border py-2 px-4 '>Slug</div>
-                    <div className='flex-[0.25] border py-2 px-4 flex gap-2 text-white'>
-                        <div className='bg-[#edc755] px-2 py-0.5 cursor-pointer'>
-                            <div>Edit</div>
-                        </div>
-                        <div className='bg-red-600 px-2 py-0.5 cursor-pointer'>
-                            <div>Hapus</div>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex justify-between font-medium'>
-                    <div className='flex-[0.05] border py-2 px-4 '>7</div>
-                    <div className='flex-[0.22] border py-2 px-4 '>Kategori</div>
-                    <div className='flex-[0.48] border py-2 px-4 '>Slug</div>
-                    <div className='flex-[0.25] border py-2 px-4 flex gap-2 text-white'>
-                        <div className='bg-[#edc755] px-2 py-0.5 cursor-pointer'>
-                            <div>Edit</div>
-                        </div>
-                        <div className='bg-red-600 px-2 py-0.5 cursor-pointer'>
-                            <div>Hapus</div>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex justify-between font-medium'>
-                    <div className='flex-[0.05] border py-2 px-4 '>8</div>
-                    <div className='flex-[0.22] border py-2 px-4 '>Kategori</div>
-                    <div className='flex-[0.48] border py-2 px-4 '>Slug</div>
-                    <div className='flex-[0.25] border py-2 px-4 flex gap-2 text-white'>
-                        <div className='bg-[#edc755] px-2 py-0.5 cursor-pointer'>
-                            <div>Edit</div>
-                        </div>
-                        <div className='bg-red-600 px-2 py-0.5 cursor-pointer'>
-                            <div>Hapus</div>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex justify-between font-medium'>
-                    <div className='flex-[0.05] border py-2 px-4 '>9</div>
-                    <div className='flex-[0.22] border py-2 px-4 '>Kategori</div>
-                    <div className='flex-[0.48] border py-2 px-4 '>Slug</div>
-                    <div className='flex-[0.25] border py-2 px-4 flex gap-2 text-white'>
-                        <div className='bg-[#edc755] px-2 py-0.5 cursor-pointer'>
-                            <div>Edit</div>
-                        </div>
-                        <div className='bg-red-600 px-2 py-0.5 cursor-pointer'>
-                            <div>Hapus</div>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex justify-between font-medium'>
-                    <div className='flex-[0.05] border py-2 px-4 '>10</div>
-                    <div className='flex-[0.22] border py-2 px-4 '>Kategori</div>
-                    <div className='flex-[0.48] border py-2 px-4 '>Slug</div>
-                    <div className='flex-[0.25] border py-2 px-4 flex gap-2 text-white'>
-                        <div className='bg-[#edc755] px-2 py-0.5 cursor-pointer'>
-                            <div>Edit</div>
-                        </div>
-                        <div className='bg-red-600 px-2 py-0.5 cursor-pointer'>
-                            <div>Hapus</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='mt-4 flex justify-between'>
-                <div>Showing 1 to 10 of 10 entries</div>
-                <div className='flex gap-2 items-center'>
-                    <div className='cursor-pointer'>Previous</div>
-                    <div className='bg-[#3bc0c3] px-2 py-1 text-white'>
-                        <div>1</div>
-                    </div>
-                    <div className='cursor-pointer'>Next</div>
                 </div>
             </div>
         </div>
